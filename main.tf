33f9b64d4024dfd96957571896f3cf647d7a0359
Calvin Cunningham <105311977+calvinjrcun77@users.noreply.github.com>
2023-04-17 00:23:43 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "azurerm_linux_web_app" "webapp" {
 #  Deploy code from a public GitHub repo
 resource "azurerm_app_service_source_control" "sourcecontrol" {
   app_id             = azurerm_linux_web_app.webapp.id
-  repo_url           = "https://github.com/calvinjrcun77/Html-Practicetest.git"
+  repo_url           = "https://github.com/"
   branch             = "master"
   use_manual_integration = true
   use_mercurial      = false
